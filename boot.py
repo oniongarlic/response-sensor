928c0a8fc3d3f70cd104a3305ae8de9399673f0a
Kaj-Michael Lang <milang@tal.org>
2023-01-05 14:20:28 +0200

    Increase wait to 20 seconds

--- boot.py
+++ boot.py
@@ -1,14 +1,14 @@
 import pycom
 import time
 
-# Wait 12 seconds for host system to boot up
+# Wait 20 seconds for host system to boot up
 
 pycom.heartbeat(False)
 
 for de in range (1,10):
  pycom.rgbled(0x7f0000)
  time.sleep(1)
- pycom.rgbled(0x2f7f000)
- time.sleep(0.2)
+ pycom.rgbled(0x2f7f00)
+ time.sleep(1)
 
 pycom.rgbled(0x00ff00)
