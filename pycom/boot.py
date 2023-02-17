fc86565f5013cb9f393caa5b2c90f34be66d0766
Kaj-Michael Lang <milang@tal.org>
2023-02-17 13:04:54 +0200

    Make sure bt is off, add dot progress

--- pycom/boot.py
+++ pycom/boot.py
@@ -1,5 +1,6 @@
 import pycom
 import time
+from network import Bluetooth
 
 # Wait 20 seconds for host system to boot up
 
@@ -7,11 +8,16 @@ print("ResponseBoot-v1")
 print("Waiting for host system to start...")
 pycom.heartbeat(False)
 
+# Make sure bt is disable
+bluetooth = Bluetooth()
+bluetooth.deinit()
+
 for de in range (1,10):
  pycom.rgbled(0x7f0000)
  time.sleep(1)
  pycom.rgbled(0x2f7f00)
  time.sleep(1)
+ print(".")
 
 pycom.rgbled(0x00ff00)
 print("...done")
